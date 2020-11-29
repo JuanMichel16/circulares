3762ebcf03ee004d8cca61e734baaa40dd8f5723
Juan Manuel Michel Vargas <juan.michel16@outlook.com>
2020-11-28 20:10:49 -0600

    iniciando trabajo

---
+++ base.js
@@ -0,0 +1,12 @@
+class Base {
+    /**
+     * 
+     * @param {String} nombre 
+     * @param {Number} minutos 
+     */
+
+    constructor(nombre, minutos) {
+        this.nombre = nombre;
+        this.minutos = minutos
+    }
+}
---
+++ ruta.js
@@ -0,0 +1,22 @@
+class Ruta {
+    constructor(){
+        this.anterior = null;
+        this.siguiente = null;
+        this.inicio = null;
+    }
+
+    agregar(nodo) {
+        if(this.inicio === null) {
+            this.inicio = nodo;
+        } else {
+            let aux = this.inicio;
+
+            while(aux.siguiente != null) {
+                aux = aux.siguiente
+            }
+
+            aux.siguiente = nodo;
+            aux.siguiente.anterior = aux;
+        }
+    }
+}
